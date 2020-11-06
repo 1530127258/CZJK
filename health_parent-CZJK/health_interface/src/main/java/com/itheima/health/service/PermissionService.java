3229d49a37b0fd7ff78e1cd68b0e8ea408fd3f01
root <1530127258@qq.com>
2020-11-06 10:36:56 +0800

    系统设置-权限管理的增删查改,及校验增加或编辑是否有重名

---
+++ health_parent-CZJK/health_interface/src/main/java/com/itheima/health/service/PermissionService.java
@@ -0,0 +1,28 @@
+package com.itheima.health.service;
+
+import com.itheima.health.entity.PageResult;
+import com.itheima.health.entity.QueryPageBean;
+import com.itheima.health.pojo.Permission;
+
+
+public interface PermissionService {
+
+
+    //分页查询
+    PageResult<Permission> findPage(QueryPageBean queryPageBean);
+
+
+    //新增
+    boolean add(Permission permission);
+
+
+    //删除
+    boolean deleteById(int id);
+
+    //编辑回显
+    Permission findById(int id);
+
+
+    //编辑修改提交
+    boolean update(Permission permission);
+}
